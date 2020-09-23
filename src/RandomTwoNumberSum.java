5fc1ffb81eb3a95989663ea6eaa79519ea77da30
kimhankyeol <kimhk1030@naver.com>
2020-09-23 21:37:53 +0900

    first commit

---
+++ src/RandomTwoNumberSum.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+public class RandomTwoNumberSum {
+    public TreeSet<Integer> solution(int[] numbers) {
+        int sum = 0;
+        //트리셋 오름차순 저장
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for(int i=0 ; i<numbers.length ; i++){
+            for(int k=i+1; k<numbers.length;k++){
+                sum = numbers[i]+numbers[k];
+                treeSet.add(sum);
+            }
+        }
+        return treeSet;
+    }
+}
